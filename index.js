e92d62302a79172c4e4fe406a1112223145f0ab6
Satvik Daga <dagasatvik10@gmail.com>
2023-07-10 04:29:31 +0530

    feat: add redux store

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
  * @format
  */
 
+import React, {StrictMode} from 'react';
 import {AppRegistry} from 'react-native';
+import {Provider} from 'react-redux';
 
 import App from './App';
 import {name as appName} from './app.json';
+import {store} from './src/app/store';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => () => (
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+));
