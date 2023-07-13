a271480f75dfa596df6ebea8c2027f38843cfce2
Satvik Daga <dagasatvik10@gmail.com>
2023-07-13 14:13:31 +0530

    feat: make user image in header pressable

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,17 +1,21 @@
 import React, {FC} from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, Pressable, Text, View} from 'react-native';
 
-import {selectUser} from '../../../features/Login/authSlice';
-import {useAppSelector} from '../../../app/hooks';
+import {selectUser, signOut} from '../../../features/Login/authSlice';
+import {useAppDispatch, useAppSelector} from '../../../app/hooks';
 import {selectCurrentActivities} from '../../../features/Activities/activitySlice';
 
 const Header: FC = () => {
+  const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
   const currentActivities = useAppSelector(selectCurrentActivities);
   return (
     user && (
       <View className="flex flex-row justify-between pb-2">
-        <View className="flex flex-row w-1/3 justify-start items-center">
+        {/* FIXME: remove sign out and navigate to points history screen */}
+        <Pressable
+          className="flex flex-row w-1/3 justify-start items-center"
+          onPress={() => dispatch(signOut())}>
           <Image
             source={{uri: user.photoURL!}}
             className="w-[50px] h-[50px] rounded-full"
@@ -20,7 +24,7 @@ const Header: FC = () => {
             <Text className="text-sm">Welcome</Text>
             <Text className="font-medium text-base">{user.displayName}</Text>
           </View>
-        </View>
+        </Pressable>
         <View className="flex flex-row w-1/3 justify-end items-center">
           <View className="flex flex-col justify-center items-center mr-1 text-black">
             <Text className="font-bold text-base">
