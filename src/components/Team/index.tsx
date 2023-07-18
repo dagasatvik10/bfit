19035edc74e7c4917c0f1a922515395dfb450e57
Satvik Daga <dagasatvik10@gmail.com>
2023-07-18 22:15:02 +0530

    feat: change team pill ui

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {Image, Text, View} from 'react-native';
+import {IconButton} from 'react-native-paper';
 
 interface Props {
   name: string;
@@ -11,19 +12,24 @@ interface Props {
 export const TeamPill: FC<Props> = ({name, points, index, isCurrent}) => {
   return (
     <View
-      className={`my-4 flex flex-row justify-start items-center rounded-xl w-full h-16 border-2 border-[#e5e5e5] ${
+      className={`my-4 flex flex-row justify-between items-center rounded-xl w-full h-16 border-2 border-[#e5e5e5] ${
         isCurrent ? 'bg-[#a2ece9]' : 'bg-white'
       }`}>
-      <View className="flex flex-col justify-center items-center rounded-full bg-[#f7b500] h-10 w-10 mx-4">
-        {index !== 0 ? (
-          <Text className="text-sm text-center font-bold">{index + 1}</Text>
-        ) : (
-          <Image source={require('../../assets/images/crown.webp')} />
-        )}
+      <View className="flex-1 flex-row justify-start items-center mx-4">
+        <View className="flex flex-col justify-center items-center rounded-full bg-[#f7b500] h-10 w-10">
+          {index !== 0 ? (
+            <Text className="text-sm text-center font-bold">{index + 1}</Text>
+          ) : (
+            <Image source={require('../../assets/images/crown.webp')} />
+          )}
+        </View>
+        <View className="flex-2 flex-col items-start justify-center px-2">
+          <Text className="text-base text-black font-bold">{name}</Text>
+          <Text className="text-black text-sm">{points} Points</Text>
+        </View>
       </View>
-      <View className="flex-2 flex-col items-start justify-center">
-        <Text className="text-base text-black font-bold">{name}</Text>
-        <Text className="text-black text-sm">{points} Points</Text>
+      <View className="flex-1 flex-row justify-end items-center">
+        <IconButton icon="chevron-right" />
       </View>
     </View>
   );
