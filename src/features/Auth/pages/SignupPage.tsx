21030a298bb71c509e7dea2affa3181348df0273
Satvik Daga <dagasatvik10@gmail.com>
2023-08-05 18:44:03 +0530

    feat: add validation in signup form

--- src/features/Auth/pages/SignupPage.tsx
+++ src/features/Auth/pages/SignupPage.tsx
@@ -1,11 +1,18 @@
 import React, {FC, useCallback, useState} from 'react';
 import {Pressable, StyleSheet, Text, View} from 'react-native';
-import {Button, TextInput} from 'react-native-paper';
+import {Button, HelperText, TextInput} from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 import {AuthStackParamList} from '../../../navigation/AuthStack';
 import {useCreateUserMutation} from '../../../slices/userSlice';
+import {validateEmail, validateName, validatePassword} from '../utils';
+
+interface SignupError {
+  name?: string;
+  email?: string;
+  password?: string;
+}
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
@@ -14,14 +21,23 @@ const SignupPage: FC<Props> = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [hidePassword, setHidePassword] = useState(true);
+  const [error, setError] = useState<SignupError>({});
 
   const [createUser] = useCreateUserMutation();
 
   const handleSignup = useCallback(async () => {
-    if (name && email && password) {
+    if (!error.email && !error.password && !error.name) {
       await createUser({name, email, password}).unwrap();
     }
-  }, [createUser, name, email, password]);
+  }, [
+    error.email,
+    error.password,
+    error.name,
+    createUser,
+    name,
+    email,
+    password,
+  ]);
 
   return (
     <SafeAreaView className="container flex-1">
@@ -41,7 +57,11 @@ const SignupPage: FC<Props> = ({navigation}) => {
             autoCapitalize="words"
             value={name}
             onChangeText={text => setName(text)}
+            onBlur={() => setError({name: validateName(name)})}
           />
+          <HelperText type="error" visible={error.name !== undefined}>
+            {error.name}
+          </HelperText>
           <TextInput
             textColor="#212121"
             outlineColor="#9e9e9e"
@@ -53,7 +73,11 @@ const SignupPage: FC<Props> = ({navigation}) => {
             autoCapitalize="none"
             value={email}
             onChangeText={text => setEmail(text)}
+            onBlur={() => setError({email: validateEmail(email)})}
           />
+          <HelperText type="error" visible={error.email !== undefined}>
+            {error.email}
+          </HelperText>
           <TextInput
             textColor="#212121"
             outlineColor="#9e9e9e"
@@ -72,8 +96,20 @@ const SignupPage: FC<Props> = ({navigation}) => {
             autoCapitalize="none"
             secureTextEntry={hidePassword}
             onChangeText={text => setPassword(text)}
+            onBlur={() => setError({password: validatePassword(password)})}
           />
+          <HelperText type="error" visible={error.password !== undefined}>
+            {error.password}
+          </HelperText>
           <Button
+            disabled={
+              !email ||
+              !password ||
+              !name ||
+              !!error.email ||
+              !!error.password ||
+              !!error.name
+            }
             onPress={handleSignup}
             className="my-4"
             mode="contained"
--- src/features/Auth/utils/email.ts
+++ src/features/Auth/utils/email.ts
@@ -1,8 +1,11 @@
 export const validateEmail = (email: string) => {
+  const re = /\S+@\S+\.\S+/;
+  if (!re.test(email)) {
+    return 'Please enter a valid email address';
+  }
+
   const splitEmail = email.split('@');
   if (!splitEmail[1] || splitEmail[1].includes('fortum')) {
     return 'Please use your personal email address';
   }
-  const re = /\S+@\S+\.\S+/;
-  return re.test(email) ? '' : 'Please enter a valid email address';
 };
