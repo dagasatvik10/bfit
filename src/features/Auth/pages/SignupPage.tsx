20e4cf323e83e3c3d5405c907e5b72e5d515f13d
Satvik Daga <dagasatvik10@gmail.com>
2023-08-13 23:34:56 +0530

    fix: set email to lower case in register

--- src/features/Auth/pages/SignupPage.tsx
+++ src/features/Auth/pages/SignupPage.tsx
@@ -78,7 +78,7 @@ const SignupPage: FC<Props> = ({navigation}) => {
             inputMode="email"
             autoCapitalize="none"
             value={email}
-            onChangeText={text => setEmail(text)}
+            onChangeText={text => setEmail(text.toLowerCase())}
           />
           <TextInput
             textColor="#212121"
